26be62a556c2aec4633f7891a74f5de768262a98
neillturner <neillwturner@gmail.com>
2016-05-13 11:59:13 +0100

    remve utf8

--- Rakefile
+++ Rakefile
@@ -1,5 +1,3 @@
-# encoding: UTF-8
-
 require 'rubocop/rake_task'
 require 'rspec/core/rake_task'
 
--- lib/kitchen/verifier/serverspec_version.rb
+++ lib/kitchen/verifier/serverspec_version.rb
@@ -1,5 +1,3 @@
-# encoding: UTF-8
-
 module Kitchen
   module Verifier
     SERVERSPEC_VERSION = '0.4.1'.freeze
